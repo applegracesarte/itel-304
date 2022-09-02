dce11ca16173b421e0296959ce8735e9d4c587bd
applegracesarte <102335812+applegracesarte@users.noreply.github.com>
2022-09-02 15:03:00 +0800

    Add files via upload

---
+++ activity2/Activity2#2.py
@@ -0,0 +1,5 @@
+b = float(input('Enter base of the triangle :'))
+h = float(input('Enter height of the triangle :'))
+
+area = (b * h) / 2
+print( 'The area of the triangle is %0.2f' % area )
---
+++ activity2/Activity2-#1.py
@@ -0,0 +1,4 @@
+addend1 = int(input("first number is: "))
+addend2 = int(input("second number is: "))
+sum = addend1 + addend2
+print(sum)
